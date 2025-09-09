e2775f78c144da35041cfe0788b20d7d65f38e85
Niket <niket.rathod@gmail.com>
2025-09-08 23:13:50 -0400

    fixed website description

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -8,7 +8,7 @@ function About() {
             </div>
             <div className="about-content">
                 <p>
-                    I'm a 3rd year CS student with a focus on cloud services. Love learning and always looking to improve. In my downtime I like to make music and travel.
+                    I'm a 4th year CS student with a focus on cloud services. Love learning and always looking to improve. In my downtime I like to make music and travel.
                 </p>
 
             </div>
